4c5a72bf17218f468128ddb4bffbdba70d20cfc3
BLCK <123077751+BLCK-B@users.noreply.github.com>
2024-11-23 08:55:28 +0100

    caching dev fix

--- vue/electron-main.js
+++ vue/electron-main.js
@@ -45,7 +45,8 @@ function createWindow() {
     return { action: "deny" };
   });
 
-  win.loadURL("http://localhost:57782");
+  if (process.env.NODE_ENV !== "development") win.loadURL("http://localhost:57782");
+  else win.loadURL("http://localhost:57782", { extraHeaders: "Cache-Control: no-cache" });
 
   win.once("ready-to-show", () => {
     win.show();

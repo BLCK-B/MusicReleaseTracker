57eeba23af35edd4280cbbc7724b92dcbaaf8b87
BLCK <123077751+BLCK-B@users.noreply.github.com>
2025-04-11 15:00:28 +0200

    composition rewrite: SourceMenu.vue

--- vue/src/components/Bar/SourceMenu.vue
+++ vue/src/components/Bar/SourceMenu.vue
@@ -13,133 +13,131 @@
       <img v-else-if="primaryColor === 'light'" class="imageSettings" src="../icons/optionslight.png" alt="Settings" />
     </button>
     <button
-      @click="clickScrape()"
-      @mouseover="scrapeHover()"
-      @mouseleave="scrapeMouseOff()"
+      @click="clickScrape"
+      @mouseover="scrapeHover"
+      @mouseleave="scrapeMouseOff"
       class="scrapeButton"
       :class="{ scrapeActive: isActive }">
       <img class="imageScrape" src="../icons/refreshuniversal.png" alt="Refresh" />
     </button>
 
     <transition name="fade">
-      <div class="scrapenotice" @mouseover="scrapeMouseOff()" v-if="scrapeDateInfo">
+      <div class="scrapenotice" @mouseover="scrapeMouseOff" v-if="scrapeDateInfo">
         <p>Last scrape: {{ scrapeLast }}</p>
       </div>
     </transition>
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref, computed, watch, onBeforeMount } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
 import axios from "axios";
-import { mapState } from "vuex";
 
-export default {
-  data() {
-    return {
-      activeTab: "",
-      eventSource: null,
-      scrapeDateInfo: false,
-      scrapeLast: "-",
-      isActive: false,
-    };
-  },
-  computed: {
-    ...mapState(["sourceTab", "allowButtons", "primaryColor", "selectedArtist"]),
-  },
-  // load last clicked tab, otherwise combview as default, load scrapeLast time
-  created() {
-    this.activeTab = this.sourceTab;
-    axios
-      .post("/api/fillCombview")
-      .catch((error) => {
-        console.error(error);
-      })
-      .then(() => {
-        if (this.sourceTab === "") this.setStoreTab("combview");
-        else this.handleSourceClick(this.sourceTab);
-      });
-    axios.get("/api/getScrapeDate").then((response) => {
-      this.scrapeLast = response.data;
+const activeTab = ref("");
+const eventSource = ref(null);
+const scrapeDateInfo = ref(false);
+const scrapeLast = ref("-");
+const isActive = ref(false);
+
+const router = useRouter();
+const store = useStore();
+const sourceTab = computed(() => store.state.sourceTab);
+const allowButtons = computed(() => store.state.allowButtons);
+const primaryColor = computed(() => store.state.primaryColor);
+const selectedArtist = computed(() => store.state.selectedArtist);
+
+// load last clicked tab, otherwise combview as default, load scrapeLast time
+onBeforeMount(() => {
+  activeTab.value = sourceTab.value;
+  axios
+    .post("/api/fillCombview")
+    .catch((error) => {
+      console.error(error);
+    })
+    .then(() => {
+      if (sourceTab.value === "") setStoreTab("combview");
+      else handleSourceClick(sourceTab.value);
     });
-  },
-  // trigger handleSourceClick if tab is not combview
-  watch: {
-    sourceTab(tabValue) {
-      this.activeTab = tabValue;
-      if (tabValue) this.handleSourceClick(tabValue);
-    },
-  },
-  methods: {
-    // set store tab, trigger handleSourceClick
-    setStoreTab(source) {
-      if (this.sourceTab === source) source = "combview";
-      this.$store.commit("SET_SOURCE_TAB", source);
-    },
-    // load respective table
-    handleSourceClick(source) {
-      axios
-        .post("/api/getTableData", { source: source, artist: this.selectedArtist })
-        .then((response) => {
-          this.$store.commit("SET_TABLE_CONTENT", response.data);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    },
-    // trigger scraping or cancel it, SSE listener for progressbar
-    clickScrape() {
-      this.scrapeDateInfo = false;
-      const allowButtons = this.allowButtons;
-      if (!allowButtons) {
-        this.eventSource.close;
-        axios.post("/api/cancelScrape").then(() => {
-          this.$store.commit("SET_ALLOW_BUTTONS", true);
-          this.isActive = false;
-        });
-      } else {
-        this.$store.commit("SET_ALLOW_BUTTONS", false);
-        this.isActive = true;
-        this.eventSource = new EventSource("http://localhost:57782/progress");
-        this.eventSource.onmessage = (event) => {
-          const progress = parseFloat(event.data);
-          this.$store.commit("SET_PROGRESS", progress);
-        };
+  axios.get("/api/getScrapeDate").then((response) => {
+    scrapeLast.value = response.data;
+  });
+});
 
-        axios.post("/api/clickScrape").then(() => {
-          this.isActive = false;
-          this.$store.commit("SET_ALLOW_BUTTONS", true);
-          this.eventSource.close();
+// trigger handleSourceClick if tab is not combview
+watch(sourceTab, (tabValue) => {
+  activeTab.value = tabValue;
+  if (tabValue) handleSourceClick(tabValue);
+});
 
-          const currentTime = new Date();
-          let time = `${currentTime.getDate().toString().padStart(2, "0")}.${(currentTime.getMonth() + 1)
-            .toString()
-            .padStart(2, "0")} ${currentTime.getHours().toString().padStart(2, "0")}:${currentTime
-            .getMinutes()
-            .toString()
-            .padStart(2, "0")}`;
+// set store tab, trigger handleSourceClick
+const setStoreTab = (source) => {
+  if (sourceTab.value === source) source = "combview";
+  store.commit("SET_SOURCE_TAB", source);
+};
+// load respective table
+const handleSourceClick = (source) => {
+  axios
+    .post("/api/getTableData", { source: source, artist: selectedArtist.value })
+    .then((response) => {
+      store.commit("SET_TABLE_CONTENT", response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+// trigger scraping or cancel it, SSE listener for progressbar
+const clickScrape = () => {
+  scrapeDateInfo.value = false;
+  if (!allowButtons.value) {
+    eventSource.value.close;
+    axios.post("/api/cancelScrape").then(() => {
+      store.commit("SET_ALLOW_BUTTONS", true);
+      isActive.value = false;
+    });
+  } else {
+    store.commit("SET_ALLOW_BUTTONS", false);
+    isActive.value = true;
+    eventSource.value = new EventSource("http://localhost:57782/progress");
+    eventSource.value.onmessage = (event) => {
+      const progress = parseFloat(event.data);
+      store.commit("SET_PROGRESS", progress);
+    };
 
-          this.scrapeLast = time;
-          this.scrapeDateInfo = true;
-          this.handleSourceClick("combview");
+    axios.post("/api/clickScrape").then(() => {
+      isActive.value = false;
+      store.commit("SET_ALLOW_BUTTONS", true);
+      eventSource.value.close();
 
-          axios.post(`/api/setSetting`, { name: "lastScrape", value: time }).catch((error) => {
-            console.error(error);
-          });
-        });
-      }
-    },
-    scrapeHover() {
-      this.scrapeDateInfo = true;
-    },
-    scrapeMouseOff() {
-      this.scrapeDateInfo = false;
-    },
-    // open settings
-    openSettings() {
-      this.$router.push("/settings");
-      this.$store.commit("SET_SETTINGS_OPEN", true);
-    },
-  },
+      const currentTime = new Date();
+      const time = `${currentTime.getDate().toString().padStart(2, "0")}.${(currentTime.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")} ${currentTime.getHours().toString().padStart(2, "0")}:${currentTime
+        .getMinutes()
+        .toString()
+        .padStart(2, "0")}`;
+
+      scrapeLast.value = time;
+      scrapeDateInfo.value = true;
+      handleSourceClick("combview");
+
+      axios.post(`/api/setSetting`, { name: "lastScrape", value: time }).catch((error) => {
+        console.error(error);
+      });
+    });
+  }
+};
+const scrapeHover = () => {
+  scrapeDateInfo.value = true;
+};
+const scrapeMouseOff = () => {
+  scrapeDateInfo.value = false;
+};
+// open settings
+const openSettings = () => {
+  router.push("/settings");
+  store.commit("SET_SETTINGS_OPEN", true);
 };
 </script>
 

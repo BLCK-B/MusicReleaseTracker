ec23f43143cda0bb0678b706577506e46c503f16
BLCK <123077751+BLCK-B@users.noreply.github.com>
2025-05-05 14:47:54 +0200

    composition rewrite: SettingsAppearance.vue, SettingsDangerZone.vue

--- vue/src/components/Settings/SettingsDangerZone.vue
+++ vue/src/components/Settings/SettingsDangerZone.vue
@@ -1,61 +1,61 @@
+<!--
+  -         MusicReleaseTracker
+  -         Copyright (C) 2023 - 2025 BLCK
+  -         This program is free software: you can redistribute it and/or modify
+  -         it under the terms of the GNU General Public License as published by
+  -         the Free Software Foundation, either version 3 of the License, or
+  -         (at your option) any later version.
+  -         This program is distributed in the hope that it will be useful,
+  -         but WITHOUT ANY WARRANTY; without even the implied warranty of
+  -         MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  -         GNU General Public License for more details.
+  -         You should have received a copy of the GNU General Public License
+  -         along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  -->
+
 <template>
   <p class="title">Danger zone</p>
   <div class="dangercont">
-    <button v-if="settingsProtection" @click="resetSettings()">Reset settings</button>
-    <button v-if="!settingsProtection" @click="resetSettings()" @mouseleave="resetProtection()">confirm</button>
+    <button v-if="settingsProtection" @click="resetSettings">Reset settings</button>
+    <button v-if="!settingsProtection" @click="resetSettings" @mouseleave="resetProtection">confirm</button>
 
-    <button v-if="dbProtection" @click="resetDB()">Reset database</button>
-    <button v-if="!dbProtection" @click="resetDB()" @mouseleave="resetProtection()">confirm</button>
+    <button v-if="dbProtection" @click="resetDB">Reset database</button>
+    <button v-if="!dbProtection" @click="resetDB" @mouseleave="resetProtection">confirm</button>
   </div>
 </template>
 
-<script>
+<script setup>
+import {ref} from "vue";
 import axios from "axios";
 
-export default {
-  data() {
-    return {
-      settingsProtection: true,
-      dbProtection: true,
-    };
-  },
-  methods: {
-    // default the settings
-    resetSettings() {
-      console.log("refistedterd");
-      if (this.settingsProtection == true) {
-        this.settingsProtection = false;
-      } else {
-        axios
-          .post("/api/resetSettings")
-          .then(() => {
-            this.clickClose();
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      }
-    },
-    // default database
-    resetDB() {
-      if (this.dbProtection == true) {
-        this.dbProtection = false;
-      } else {
-        axios
-          .post("/api/resetDB")
-          .then(() => {
-            this.clickClose();
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      }
-    },
-    resetProtection() {
-      this.settingsProtection = true;
-      this.dbProtection = true;
-    },
-  },
+const settingsProtection = ref(true);
+const dbProtection = ref(true);
+
+// default the settings
+const resetSettings = () => {
+  if (settingsProtection.value === true) {
+    settingsProtection.value = false;
+  } else {
+    axios.post("/api/resetSettings").catch((error) => {
+      console.error(error);
+    });
+  }
+};
+
+// default the database
+const resetDB = () => {
+  if (dbProtection.value === true) {
+    dbProtection.value = false;
+  } else {
+    axios.post("/api/resetDB").catch((error) => {
+      console.error(error);
+    });
+  }
+};
+
+const resetProtection = () => {
+  settingsProtection.value = true;
+  dbProtection.value = true;
 };
 </script>
 

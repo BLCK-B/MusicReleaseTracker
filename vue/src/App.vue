ffdcd28bf2255bce292fd6c94a51c5a8ee858de9
BLCK <123077751+BLCK-B@users.noreply.github.com>
2025-06-27 14:18:05 +0200

    App.vue composition api

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,94 +2,92 @@
   <RouterView />
 </template>
 
-<script>
+<script setup>
 import axios from "axios";
-import { mapState } from "vuex";
+import { ref, computed, watch, onMounted } from "vue";
+import { useStore } from "vuex";
 axios.defaults.baseURL = "http://localhost:57782";
 
-export default {
-  data() {
-    return {
-      appliedStyles: [],
-      theme: "",
-      accent: "",
-    };
-  },
-  computed: {
-    ...mapState(["primaryColor", "accentColor"]),
-  },
-  watch: {
-    // load themes whenever store theme/accent change
-    primaryColor(theme) {
-      this.theme = theme;
-      this.applyTheme(theme, this.accent);
-    },
-    accentColor(accent) {
-      this.accent = accent;
-      this.applyTheme(this.theme, accent);
-    },
-  },
-  created() {
-    this.loadTheme();
-    this.detectTheme();
-  },
-  methods: {
-    loadTheme() {
-      // on start, load themes from config
-      axios
-        .get("/api/getThemeConfig")
-        .then((response) => {
-          this.$store.commit("SET_PRIMARY_COLOR", response.data.theme);
-          this.$store.commit("SET_ACCENT_COLOR", response.data.accent);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    },
-    detectTheme() {
-      // detecting system theme on load
-      axios
-        .get("/api/settingsOpened")
-        .then((response) => {
-          const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
-          if (response.data.autoTheme === "true") {
-            if (prefersDarkMode.matches) this.$store.commit("SET_PRIMARY_COLOR", "black");
-            else this.$store.commit("SET_PRIMARY_COLOR", "light");
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    },
-    applyTheme(theme, accent) {
-      theme = theme.toLowerCase();
-      accent = accent.toLowerCase();
-      // remove previously applied css
-      this.appliedStyles.forEach((style) => {
-        style.remove();
-      });
-      this.appliedStyles = [];
-      let themePath;
-      let linkElement;
+const store = useStore();
 
-      if (theme !== "") {
-        themePath = `./theme-${theme}.css`;
-        linkElement = document.createElement("link");
-        linkElement.rel = "stylesheet";
-        linkElement.href = themePath;
-        document.head.appendChild(linkElement);
-        this.appliedStyles.push(linkElement);
+const appliedStyles = ref([]);
+const theme = ref("");
+const accent = ref("");
+
+const primaryColor = computed(() => store.state.primaryColor);
+const accentColor = computed(() => store.state.accentColor);
+
+watch(primaryColor, (newTheme) => {
+  theme.value = newTheme;
+  applyTheme(theme.value, accent.value);
+});
+
+watch(accentColor, (newAccent) => {
+  accent.value = newAccent;
+  applyTheme(theme.value, accent.value);
+});
+
+onMounted(() => {
+  loadTheme();
+  detectTheme();
+});
+
+const loadTheme = () => {
+  // on start, load themes from config
+  axios
+    .get("/api/getThemeConfig")
+    .then((response) => {
+      store.commit("SET_PRIMARY_COLOR", response.data.theme);
+      store.commit("SET_ACCENT_COLOR", response.data.accent);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+const detectTheme = () => {
+  // detecting system theme on load
+  axios
+    .get("/api/settingsOpened")
+    .then((response) => {
+      const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+      if (response.data.autoTheme === "true") {
+        if (prefersDarkMode.matches) store.commit("SET_PRIMARY_COLOR", "black");
+        else store.commit("SET_PRIMARY_COLOR", "light");
       }
-      if (accent !== "") {
-        themePath = `./accent-${accent}.css`;
-        linkElement = document.createElement("link");
-        linkElement.rel = "stylesheet";
-        linkElement.href = themePath;
-        document.head.appendChild(linkElement);
-        this.appliedStyles.push(linkElement);
-      }
-    },
-  },
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+const applyTheme = (newTheme, newAccent) => {
+  theme.value = newTheme.toLowerCase();
+  accent.value = newAccent.toLowerCase();
+  // remove previously applied css
+  appliedStyles.value.forEach((style) => {
+    style.remove();
+  });
+  appliedStyles.value = [];
+  let themePath;
+  let linkElement;
+
+  if (newTheme !== "") {
+    themePath = `./theme-${newTheme}.css`;
+    linkElement = document.createElement("link");
+    linkElement.rel = "stylesheet";
+    linkElement.href = themePath;
+    document.head.appendChild(linkElement);
+    appliedStyles.value.push(linkElement);
+  }
+  if (newAccent !== "") {
+    themePath = `./accent-${newAccent}.css`;
+    linkElement = document.createElement("link");
+    linkElement.rel = "stylesheet";
+    linkElement.href = themePath;
+    document.head.appendChild(linkElement);
+    appliedStyles.value.push(linkElement);
+  }
 };
 </script>
 

5955536724980790b64d4b2c66c87691784cc85d
BLCK <123077751+BLCK-B@users.noreply.github.com>
2025-12-14 08:43:49 +0100

    11.1.0 improvements (#232)

--- vue/src/pages/MainPage.vue
+++ vue/src/pages/MainPage.vue
@@ -2,39 +2,39 @@
   <div class="app">
     <!-- sidebar -->
     <div class="list">
-      <ArtistList />
+      <ArtistList/>
     </div>
 
     <div class="maincontent">
       <!-- top bar -->
       <div v-if="!previewVis" class="topbar">
-        <SourceMenu />
+        <SourceMenu/>
       </div>
 
       <!-- content -->
       <div class="sourcetable">
-        <SourceTable />
+        <SourceTable/>
       </div>
 
       <div class="dialogsurl" v-if="!previewVis">
-        <DialogsURL />
+        <DialogsURL/>
       </div>
     </div>
 
     <div class="progressbar">
-      <ProgressBar />
+      <ProgressBar/>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import * as ProgressBar from "@/components/Content/ProgressBar.vue";
-import * as ArtistList from "@/components/Artists/ArtistList.vue";
-import * as SourceMenu from "@/components/Bar/SourceMenu.vue";
-import * as SourceTable from "@/components/Content/SourceTable.vue";
-import * as DialogsURL from "@/components/Content/DialogsURL.vue";
-import { computed } from "vue";
-import { useMainStore } from "@/store/mainStore.ts";
+import ArtistList from "@/components/Artists/ArtistList.vue";
+import DialogsURL from "@/components/Content/DialogsURL.vue";
+import SourceMenu from "@/components/Bar/SourceMenu.vue";
+import SourceTable from "@/components/Content/SourceTable.vue";
+import ProgressBar from "@/components/Content/ProgressBar.vue";
+import {computed} from "vue";
+import {useMainStore} from "@/store/mainStore.ts";
 
 const store = useMainStore();
 
@@ -45,13 +45,16 @@ const previewVis = computed(() => store.previewVis);
 * {
   scrollbar-color: var(--dull-color) transparent;
 }
+
 *::-webkit-scrollbar-thumb {
   background-color: var(--dull-color);
 }
+
 *::-webkit-scrollbar {
   width: 8px;
   background: transparent;
 }
+
 .app {
   display: flex;
   position: fixed;
@@ -59,6 +62,7 @@ const previewVis = computed(() => store.previewVis);
   width: 100%;
   height: 100%;
 }
+
 .list {
   width: 175px;
   min-width: 175px;
@@ -69,27 +73,32 @@ const previewVis = computed(() => store.previewVis);
   position: relative;
   background-color: var(--subtle-color);
 }
+
 .maincontent {
   flex-grow: 1;
   display: flex;
   flex-direction: column;
 }
+
 .topbar {
   left: 5px;
   position: relative;
 }
+
 .dialogsurl {
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
 }
+
 .sourcetable {
   flex-grow: 1;
   margin-top: 6px;
   overflow-y: auto;
   margin-right: 4px;
 }
+
 .progressbar {
   position: absolute;
   bottom: 3px;

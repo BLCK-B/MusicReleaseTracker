36f18ee32c96a4909bd387adfe6f17784738694f
BLCK-B <123077751+BLCK-B@users.noreply.github.com>
2026-08-04 20:15:25 +0200

    fix mergeSongsWithinDaysApart

--- src/main/java/com/blck/MusicReleaseTracker/Scraping/Scrapers/Scraper.java
+++ src/main/java/com/blck/MusicReleaseTracker/Scraping/Scrapers/Scraper.java
@@ -95,7 +95,6 @@ public abstract class Scraper {
     public List<Song> processInfo(List<Song> songList) {
         if (songList.isEmpty()) {
             log.error(new Exception(), ErrorLogging.Severity.WARNING, "song list produced by scraper is empty");
-            return null;
         }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
